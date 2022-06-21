ccf17f1bae0f3bba220fb158f9f23821a96c5ef7
Gbengacodes <gbengaoyewole89@gmail.com>
2022-06-21 05:14:35 +0100

    delete maps

--- Desktop/new/maps.go
+++ Desktop/new/maps.go
@@ -4,9 +4,12 @@ package main
 import "fmt"
 
 func main() {
+	//initializing the make function
 	//var app = make(map[string]int)
 	//app["dividend"] = 21
 	//fmt.Println(app)
+
+	//Initializing key values in a map
 	//app := make(map[int]string)
 	//app[21] = "Facebook"
 	//app[22] = "Whatsapp"
@@ -15,10 +18,24 @@ func main() {
 	//app[25] = "Reddit"
 	//fmt.Println(app)
 
-	var app = map[string]int{
-		"k": 19,
-		"l": 20,
-		"m": 21,
-	}
-	fmt.Println(app)
+	//initializing maps using map literal
+	//var app = map[string]int{
+	//	"k": 19,
+	//	"l": 20,
+	//	"m": 21,
+	//}
+	//fmt.Println(app)
+
+	//deleting key from maps.
+	countries := make(map[int]string)
+	countries[1] = "Rwanda"
+	countries[2] = "Namibia"
+	countries[3] = "Kenya"
+	countries[4] = "Botswana"
+	countries[5] = "SouthAfrica"
+	countries[6] = "Tanzania"
+	fmt.Println(countries)
+	delete(countries, 5)
+	fmt.Println(countries)
+
 }
