44a0593c0e321ea7cf980957d29b85164a21b416
Gbengacodes <gbengaoyewole89@gmail.com>
2022-06-21 04:49:19 +0100

    maps

--- Desktop/new/maps.go
+++ Desktop/new/maps.go
@@ -7,11 +7,18 @@ func main() {
 	//var app = make(map[string]int)
 	//app["dividend"] = 21
 	//fmt.Println(app)
-	app := make(map[int]string)
-	app[21] = "Facebook"
-	app[22] = "Whatsapp"
-	app[23] = "LinkedIn"
-	app[24] = "Instagram"
-	app[25] = "Reddit"
+	//app := make(map[int]string)
+	//app[21] = "Facebook"
+	//app[22] = "Whatsapp"
+	//app[23] = "LinkedIn"
+	//app[24] = "Instagram"
+	//app[25] = "Reddit"
+	//fmt.Println(app)
+
+	var app = map[string]int{
+		"k": 19,
+		"l": 20,
+		"m": 21,
+	}
 	fmt.Println(app)
 }
