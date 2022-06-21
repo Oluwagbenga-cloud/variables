54464941217ceb826bd1e30b26467cda1c9a6c1e
Gbengacodes <gbengaoyewole89@gmail.com>
2022-06-21 04:27:25 +0100

    maps continuation

--- Desktop/new/maps.go
+++ Desktop/new/maps.go
@@ -4,8 +4,14 @@ package main
 import "fmt"
 
 func main() {
-	var app = make(map[string]int)
-	app["dividend"] = 21
+	//var app = make(map[string]int)
+	//app["dividend"] = 21
+	//fmt.Println(app)
+	app := make(map[int]string)
+	app[21] = "Facebook"
+	app[22] = "Whatsapp"
+	app[23] = "LinkedIn"
+	app[24] = "Instagram"
+	app[25] = "Reddit"
 	fmt.Println(app)
-
 }
