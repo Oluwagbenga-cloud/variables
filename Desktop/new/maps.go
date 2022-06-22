9be6a5e1ad37d46961a1dd4e30ce072251b03f88
Gbengacodes <gbengaoyewole89@gmail.com>
2022-06-22 06:10:25 +0100

    boolean map

--- Desktop/new/maps.go
+++ Desktop/new/maps.go
@@ -26,16 +26,27 @@ func main() {
 	//}
 	//fmt.Println(app)
 
-	//deleting key from maps.
-	countries := make(map[int]string)
-	countries[1] = "Rwanda"
-	countries[2] = "Namibia"
-	countries[3] = "Kenya"
-	countries[4] = "Botswana"
-	countries[5] = "SouthAfrica"
-	countries[6] = "Tanzania"
-	fmt.Println(countries)
-	delete(countries, 5)
-	fmt.Println(countries)
+	////deleting key from maps.
+	//countries := make(map[int]string)
+	//countries[1] = "Rwanda"
+	//countries[2] = "Namibia"
+	//countries[3] = "Kenya"
+	//countries[4] = "Botswana"
+	//countries[5] = "SouthAfrica"
+	//countries[6] = "Tanzania"
+	//fmt.Println(countries)
+	//delete(countries, 5)
 
+	age := map[string]int{
+
+		"Nala":   29,
+		"Kayla":  30,
+		"Nadine": 30,
+		"Busayo": 31,
+	}
+	BusayoAge, BusayoOk := age["Busayo"]
+	NalaAge, NalaOk := age["Nala"]
+
+	fmt.Println(BusayoAge, BusayoOk)
+	fmt.Println(NalaAge, NalaOk)
 }
